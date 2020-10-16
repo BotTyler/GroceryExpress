c008fab8407b6d85fa6d5a874988d7f358fd17f6
Tyler Jarboe <tyler2.jarboe@gmail.com>
2020-10-15 22:18:05 -0400

    Initial commit

---
+++ app/src/main/java/edu/floridapoly/mobiledeviceapp/fall2020/groceryexpress/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapp.fall2020.groceryexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
